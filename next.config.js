34b33f9c1466c3409fec47e76a96e6c3108837c4
EcfatumGhana <akorlicourage@gmail.com>
2024-01-12 13:44:27 +0000

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/:path*',
+                destination: '/portal/:path*',
+            }
+        ]
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/dashboard',
+                permanent: true
+            },
+            {
+                source: '/login',
+                destination: '/auth/login',
+                permanent: true
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig
