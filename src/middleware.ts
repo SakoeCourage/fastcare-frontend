3bbc42ba93ec29e29727dd9cc18897aa3b1e2cea
sakoe courage <akorlicourage@gmail.com>
2024-02-04 00:25:28 +0000

    Change to token User Auth Options

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { getSession } from 'next-auth/react'
 
 export function middleware(request: NextRequest) {
     const currentAuthToken = request.cookies.get("next-auth.session-token")?.value
-    // console.log(request.cookies)
+    console.log(request.cookies)
     if (currentAuthToken === undefined) {
         if (!request.url.endsWith("/login")) {
             return NextResponse.redirect(new URL('/login', request.url))
